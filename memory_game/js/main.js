2ab1f3bbe5bc4bbc16dde8fcff06ad62516445f2
Charles Cui <xarlze@Arkelion.local>
2019-02-17 22:53:30 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,16 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+console.log('User flipped ' + cardOne);
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardTwo);
 
-
-
-console.log("User flipped " + cardOne);
+if(cardsInPlay.length===2){
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
