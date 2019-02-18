402bd46560999b311f893e4ccb2d749682cd0b0e
Charles Cui <xarlze@Arkelion.local>
2019-02-17 23:14:50 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage:"images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage:"images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage:"images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage:"images/king-of-diamonds.png",
+},
+];
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -12,8 +33,8 @@ function checkForMatch(){
 }
 
 function flipCard(cardId) {
-	console.log("User flipped "+ cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped card with rank "+ cards[cardId].rank+ " suit "+ cards[cardId].suit+ " and image path " + cards[cardId].cardImage)
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 
